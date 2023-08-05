23342005aad714ebd88f8a5a947cb75100182260
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-05 18:40:29 +0300

    adding class and route for GET, PUT, DEL by ID. Also verify payload first

--- todosProject/urls.py
+++ todosProject/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todosapp.views import Todos_list_view
+from todosapp.views import Todos_list_view, Todos_details_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todos/', Todos_list_view.as_view(), name='Todos_list_view')
+    path('todos/', Todos_list_view.as_view(), name='Todos_list_view'),
+    path('todos/<int:pk>/', Todos_details_view.as_view(), name='Todos_details_view')
 ]
--- todosapp/views.py
+++ todosapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import JsonResponse
@@ -24,8 +24,44 @@ class Todos_list_view(View):
         request_data = json.loads(request.body)
         my_title = request_data.get('title')
 
-        if my_title:
+        if type(my_title) != str:
+            return JsonResponse({'Error': "title must be of type string"}, status=400)
+        elif len(my_title) == 0:
+            return JsonResponse({'Error': "title can't be empty string"}, status=400)
+        elif my_title:
             task = Task.objects.create(title=my_title)
             return JsonResponse({'id': task.id, 'title': task.title}, status=201)
         else:
             return JsonResponse({'Error': "title is required"}, status=400)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class Todos_details_view(View):
+    ''' class based view to handle details view of on todo'''
+
+    def get(self, request, pk):
+        ''' Handle get request to get one todo by ID'''
+        task = get_object_or_404(Task, pk=pk)
+        return JsonResponse({'id': task.id, 'title': task.title}, status=200)
+
+    def put(self, request, pk):
+        ''' Handle put request to update one todo by ID'''
+        request_data = json.loads(request.body)
+        my_title = request_data.get('title')
+        task = get_object_or_404(Task, pk=pk)
+        if my_title and type(my_title) != str:
+            return JsonResponse({'Error': "title must be of type string"}, status=400)
+        elif my_title and len(my_title) == 0:
+            return JsonResponse({'Error': "title can't be empty string"}, status=400)
+        elif my_title:
+            task.title = my_title
+            task.save()
+            return JsonResponse({'id': task.id, 'title': task.title}, status=200)
+        else:
+            return JsonResponse({'Error': "title is required"}, status=400)
+
+    def delete(self, request, pk):
+        ''' Handle delete request to remove one todo by ID' '''
+        task = get_object_or_404(Task, pk=pk)
+        task.delete()
+        return JsonResponse({'message': 'Task has been deleted successfully.'}, status=204)
