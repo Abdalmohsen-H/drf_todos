72341e8670e959c515436dcdf651437339cb331d
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-05 15:45:56 +0300

    fix csrf exempt by applying it to each instance of class using method docerator on dispatch

--- todosapp/views.py
+++ todosapp/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import JsonResponse
-from .models import Task
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from .models import Task
 import json
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class Todos_list_view(View):
     ''' class based view to handle list view of todos '''
 
@@ -17,7 +19,6 @@ class Todos_list_view(View):
         tasksList = [{'id': task.id, 'title': task.title} for task in tasks]
         return JsonResponse(tasksList, safe=False, status=200)
 
-    @csrf_exempt
     def post(self, request):
         ''' Handle post request to add todo'''
         request_data = json.loads(request.body)
