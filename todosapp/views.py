32c6c7d619e3000f228665e00374f051a6a01438
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-05 19:14:44 +0300

    black formatter

--- todosapp/views.py
+++ todosapp/views.py
@@ -9,66 +9,59 @@ from .models import Task
 import json
 
 
-@method_decorator(csrf_exempt, name='dispatch')
+@method_decorator(csrf_exempt, name="dispatch")
 class Todos_list_view(View):
-    ''' class based view to handle list view of todos '''
+    """class based view to handle list view of todos"""
 
     def get(self, request):
-        ''' Handle get request to get all todos '''
+        """Handle get request to get all todos"""
         tasks = Task.objects.all()
-        tasksList = [{'id': task.id, 'title': task.title} for task in tasks]
+        tasksList = [{"id": task.id, "title": task.title} for task in tasks]
         return JsonResponse(tasksList, safe=False, status=200)
 
     def post(self, request):
-        ''' Handle post request to add todo'''
+        """Handle post request to add todo"""
         request_data = json.loads(request.body)
-        my_title = request_data.get('title')
+        my_title = request_data.get("title")
 
         if isinstance(my_title, str) is False:
-            return JsonResponse({'Error': "title must be of type string"},
-                                status=400)
+            return JsonResponse({"Error": "title must be of type string"}, status=400)
         elif len(my_title) == 0:
-            return JsonResponse({'Error': "title can't be empty string"},
-                                status=400)
+            return JsonResponse({"Error": "title can't be empty string"}, status=400)
         elif my_title:
             task = Task.objects.create(title=my_title)
-            return JsonResponse({'id': task.id, 'title': task.title},
-                                status=201)
+            return JsonResponse({"id": task.id, "title": task.title}, status=201)
         else:
-            return JsonResponse({'Error': "title is required"}, status=400)
+            return JsonResponse({"Error": "title is required"}, status=400)
 
 
-@method_decorator(csrf_exempt, name='dispatch')
+@method_decorator(csrf_exempt, name="dispatch")
 class Todos_details_view(View):
-    ''' class based view to handle details view of on todo'''
+    """class based view to handle details view of on todo"""
 
     def get(self, request, pk):
-        ''' Handle get request to get one todo by ID'''
+        """Handle get request to get one todo by ID"""
         task = get_object_or_404(Task, pk=pk)
-        return JsonResponse({'id': task.id, 'title': task.title}, status=200)
+        return JsonResponse({"id": task.id, "title": task.title}, status=200)
 
     def put(self, request, pk):
-        ''' Handle put request to update one todo by ID'''
+        """Handle put request to update one todo by ID"""
         request_data = json.loads(request.body)
-        my_title = request_data.get('title')
+        my_title = request_data.get("title")
         task = get_object_or_404(Task, pk=pk)
         if isinstance(my_title, str) is False:
-            return JsonResponse({'Error': "title must be of type string"},
-                                status=400)
+            return JsonResponse({"Error": "title must be of type string"}, status=400)
         elif my_title and len(my_title) == 0:
-            return JsonResponse({'Error': "title can't be empty string"},
-                                status=400)
+            return JsonResponse({"Error": "title can't be empty string"}, status=400)
         elif my_title:
             task.title = my_title
             task.save()
-            return JsonResponse({'id': task.id, 'title': task.title},
-                                status=200)
+            return JsonResponse({"id": task.id, "title": task.title}, status=200)
         else:
-            return JsonResponse({'Error': "title is required"}, status=400)
+            return JsonResponse({"Error": "title is required"}, status=400)
 
     def delete(self, request, pk):
-        ''' Handle delete request to remove one todo by ID' '''
+        """Handle delete request to remove one todo by ID'"""
         task = get_object_or_404(Task, pk=pk)
         task.delete()
-        return JsonResponse({'message': 'Task deleted successfully.'},
-                            status=204)
+        return JsonResponse({"message": "Task deleted successfully."}, status=204)
