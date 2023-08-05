fcf8af252b62510734deface711ab3a37043d795
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-05 07:47:12 +0300

    Create class inside add models.py, makemigrations , create super user , register model to app's admin.py

--- todosapp/admin.py
+++ todosapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here to be able to manage it from django admin dashboard.
+from .models import Task
+
+admin.site.register(Task)
