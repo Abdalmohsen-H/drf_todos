4164f2d02cc3af7f02dd6807a7fb084da9a2b512
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-22 18:28:20 +0300

    remove unused lines from todosapp's views.py

--- todosapp/views.py
+++ todosapp/views.py
@@ -1,11 +1,9 @@
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
 
 from .models import Task
 from .utils import TodosSerializer
 
 
-# @method_decorator(csrf_exempt, name="dispatch")
 class TodosListView(generics.ListCreateAPIView):
     """generic class based view to handle list view of todos"""
 
