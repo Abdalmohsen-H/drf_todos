2aed1aada431e3abb14248c902d4a354619f7537
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-27 09:04:51 +0300

    fix typo on comment

--- todosapp/urls.py
+++ todosapp/urls.py
@@ -5,7 +5,7 @@ from .views import TodosViewSet
 
 router = DefaultRouter()
 # todos here referes to ../todos/.. -> todos resource base url that
-# will be used to generate paths based on setview actions
+# will be used to generate paths based on viewset actions
 router.register(r"todos", TodosViewSet)
 
 urlpatterns = [
