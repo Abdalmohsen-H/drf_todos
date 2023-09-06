c810ae3797d53e2baa213d9fd8079848d6f97dee
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-06 20:24:24 +0300

    move owner and updated_byy fields to serializer instead of views

--- todosapp/serializers.py
+++ todosapp/serializers.py
@@ -7,7 +7,7 @@ from .models import Task
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username"]
+        fields = ["username"]
 
 
 class TodoSerializer(serializers.ModelSerializer):
@@ -19,3 +19,19 @@ class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ["id", "title", "created_at", "updated_at", "owner", "updated_by"]
+
+    def create(self, validated_data):
+        """set the created_by and updated_by fields when a
+        new task is created."""
+        validated_data["owner"] = self.context["request"].user
+        validated_data["updated_by"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        """update the updated_by field only when an existing task
+        is updated.
+        instance here is an instance of the Task model"""
+        instance.updated_by = self.context["request"].user
+
+        instance.save()
+        return instance
--- todosapp/views.py
+++ todosapp/views.py
@@ -12,15 +12,3 @@ class TodosViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TodoSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        """override the perform_create method to set
-        the created_by and updated_by fields when a
-        new task is created."""
-        serializer.save(owner=self.request.user, updated_by=self.request.user)
-
-    def perform_update(self, serializer):
-        """override the perform_update method to update
-        the updated_by field only when an existing task
-        is updated."""
-        serializer.save(updated_by=self.request.user)
