7a4190b83c864b7ff1e3f0fcea9657edc6853e0a
Abdalmohsen-H <egywebdes@gmail.com>
2023-09-09 14:41:35 +0300

    obj's owner could only edit within 7days, But delete anytime, admins have no restrictions

--- todosapp/permissions.py
+++ todosapp/permissions.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+
+from django.utils import timezone
 from rest_framework import permissions
 
 
@@ -22,4 +25,15 @@ class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
             # if you want obj read-only access only for other signed-in users
             # return request.user.is_authenticated
 
-        return request.user.is_staff or (request.user == obj.owner)
+        if request.user.is_staff:  # ir request.user is an admin
+            return True  # allow everything for object without time restrictions
+
+        # owner only have 7 days to edit but could delete any time
+        if request.user == obj.owner:
+            # Check if the task's "created_at" date is within the last 7 days
+            seven_days_ago = timezone.now() - timedelta(days=7)
+            is_recently_created = obj.created_at >= seven_days_ago
+            return is_recently_created or (request.method == "DELETE")
+
+        else:
+            return False  # restirct others from editing or deleting
